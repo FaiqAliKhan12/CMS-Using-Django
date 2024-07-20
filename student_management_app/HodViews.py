a93bae47fff66471d4ca9dc11e4ba52980a79167
Faiq Ali Khan <faiqalikhan9@gmail.com>
2024-07-19 17:47:27 -0700

    admin home staff add,edit,manage,delete function work done

---
+++ student_management_app/HodViews.py
@@ -0,0 +1,177 @@
+
+from django.shortcuts import render, redirect 
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse 
+from django.contrib import messages 
+from django.core.files.storage import FileSystemStorage 
+from django.urls import reverse 
+from django.views.decorators.csrf import csrf_exempt 
+import json 
+  
+from .forms import AddStudentForm, EditStudentForm 
+  
+from .models import CustomUser, Staffs, Courses, Subjects, Students, SessionYearModel, FeedBackStudent, FeedBackStaffs, LeaveReportStudent, LeaveReportStaff, Attendance, AttendanceReport 
+
+def admin_home(request):
+    all_student_count = Students.objects.all().count()
+    subject_count = Subjects.objects.all().count()
+    course_count = Courses.objects.all().count()
+    staff_count = Staffs.objects.all().count()
+    course_all = Courses.objects.all()
+    course_name_list = []
+    subject_count_list = []
+    student_count_list_in_course = []
+
+    for course in course_all:
+        subjects = Subjects.objects.filter(course_id=course.id).count()
+        students = Students.objects.filter(course_id=course.id).count()
+        course_name_list.append(course.course_name)
+        subject_count_list.append(subjects)
+        student_count_list_in_course.append(students)
+
+    subject_all = Subjects.objects.all()
+    subject_list = []
+    student_count_list_in_subject = []
+    for subject in subject_all:
+        course = Courses.objects.get(id=subject.course_id.id)
+        student_count = Students.objects.filter(course_id=course.id).count()
+        subject_list.append(subject.subject_name)
+        student_count_list_in_subject.append(student_count)
+
+
+        #FOr Sttaf
+        staff_attendance_present_list=[]
+        staff_attendance_leave_list=[]
+        staff_name_list=[]
+
+        staffs = Staffs.objects.all()
+        for staff in staffs:
+            subjects_ids = Subjects.objects.filter(staff_id=staff.admin.id)
+            attendance = Attendance.objects.filter(subject_id__in=subjects_ids).count()
+            leaves = LeaveReportStaff.objects.filter(staff_id=staff.id,
+                                                     leave_status=1).count()
+            staff_attendance_present_list.append(attendance)
+            staff_attendance_leave_list.append(leaves)
+            staff_name_list.append(staff.admin.first_name)
+
+        #For Students
+
+        student_attendance_present_list=[]
+        student_attendance_leave_list=[]
+        student_name_list=[]
+
+        students= Students.objects.all()
+        for student in students:
+            attendance = AttendanceReport.objects.filter(student_id=student.id,
+                                                         status=True).count()
+            absent = AttendanceReport.objects.filter(student_id=student.id,
+                                                     status=False).count()
+            leaves = LeaveReportStudent.objects.filter(student_id=student.id,
+                                                       leave_status=1).count()
+            student_attendance_present_list.append(attendance)
+            student_attendance_leave_list.append(leaves+absent)
+            student_name_list.append(student.admin.first_name)
+
+            context = {
+                "all_student_count":all_student_count,
+                "subject_count":subject_count,
+                "course_count":course_count,
+                "staff_count":staff_count,
+                "course_name_list":course_name_list,
+                "subject_count_list":subject_count_list,
+                "student_count_list_in_course":student_count_list_in_course,
+                "subject_list":subject_list,
+                "student_count_list_in_subject":student_count_list_in_subject,
+                "staff_attendance_present_list":student_attendance_present_list,
+                "staff_attendance_leave_list":staff_attendance_leave_list,
+                 "staff_name_list": staff_name_list, 
+                 "student_attendance_present_list": student_attendance_present_list, 
+                "student_attendance_leave_list": student_attendance_leave_list, 
+                "student_name_list": student_name_list, 
+            }
+            return render(request,"hod_template/home_content.html",context)
+def add_staff(request):
+    return render(request,"hod_template/add_staff_template.html")
+
+def add_staff_save(request):
+    if request.method != "POST":
+        messages.error(request,"Invalid Method!!")
+        return redirect('add_staff')
+    else:
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        address = request.POST.get('address')
+
+        try:
+            user = CustomUser.objects.create_user(username=username,
+                                                  password=password,
+                                                  email=email,
+                                                  first_name=first_name,
+                                                  last_name=last_name,
+                                                  user_type=2)
+            user.staffs.address = address
+            user.save()
+            messages.success(request,"Staff Added Successfully!!")
+            return redirect('add_staff')
+        except:
+            messages.error(request,"Failed To Add Staff!!")
+            return redirect('add_staff')
+        
+def manage_staff(request):
+    staffs = Staffs.objects.all()
+    context = {
+        "staffs":staffs,
+    }
+    return redirect(request,"hod_template/manage_staff_template.html")
+
+def edit_staff(request,staff_id):
+    staff = Staffs.objects.get(admin=staff_id)
+
+    context = {
+        "staff":staff,
+        "id":staff_id
+    }
+    return render(request,"hod_template/edit_staff_template.html",context)
+
+def edit_staff_save(request):
+    if request.method != "POST":
+        return HttpResponse("<h2>Method Not Allowed!</h2>")
+    else:
+        staff_id = request.POST.get('staff_id')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        address = request.POST.get('address')
+
+        try:
+            #Inserting into CustomUser Model
+            user = CustomUser.objects.get(id=staff_id)
+            user.first_name = first_name
+            user.last_name = last_name
+            user.email = email 
+            user.username = username
+            user.save()
+
+            #inserting into staff model
+            staff_model = Staffs.objects.get(admin=staff_id)
+            staff_model.address = address
+            staff_model.save()
+
+            messages.success(request,"Staff Added Successfully!!!")
+            return redirect('/edit_staff/'+staff_id)
+        except:
+            messages.error(request,"Failed to Update Staff!!")
+            return redirect('/edit_staff/'+staff_id)
+        
+def delete_staff(request,staff_id):
+    staff = Staffs.objects.get(admin=staff_id)
+    try:
+        staff.delete()
+        messages.success(request,"Staff Deleted Successfully!!")
+        return redirect('manage_staff')
+    except:
+        messages.error(request,"Failed to delete Staff!!")
+        return redirect('manage_staff')
